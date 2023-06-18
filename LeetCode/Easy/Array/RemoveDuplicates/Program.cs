c6361ccf8f2ae673d850ec31b9b03a6e52808145
Svyatoslav Ryumkin <s.ryumkin@of-group.ru>
2023-06-18 12:49:14 +0300

    Remove Duplicates from Sorted Array

---
+++ LeetCode/Easy/Array/RemoveDuplicates/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 1)
+        {
+            return 1;
+        }
+        var index = 1;
+        var count = 1;
+        var previousNumber = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (previousNumber == nums[i])
+            {
+                continue;
+            }
+            count++;
+            nums[index++] = previousNumber = nums[i];
+        }
+
+        return count;
+    }
+}
