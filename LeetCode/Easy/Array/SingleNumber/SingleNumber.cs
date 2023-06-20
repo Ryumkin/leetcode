e088fae2a0881e31fff2e85427420d118f37fa1a
Svyatoslav Ryumkin <s.ryumkin@of-group.ru>
2023-06-20 22:56:13 +0300

    SingleNumber

--- LeetCode/Easy/Array/SingleNumber/SingleNumber.cs
+++ LeetCode/Easy/Array/SingleNumber/SingleNumber.cs
@@ -1,16 +1,16 @@
-﻿public class ContainsDuplicateSolution {
-    public bool ContainsDuplicate(int[] nums) {
+﻿public class Solution {
+    public int SingleNumber(int[] nums) {
         var hashset = new HashSet<int>(nums.Length);
         foreach(var item in nums){
             if(hashset.Contains(item))
             {
-                return true;
+                hashset.Remove(item);
             }
             else{   
                 hashset.Add(item);
             }
         }
 
-        return false;
+        return hashset.First();
     }
 }
