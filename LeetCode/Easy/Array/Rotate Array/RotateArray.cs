95cf1dd287a5e76b58432d46220cf902acd3545e
Svyatoslav Ryumkin <s.ryumkin@of-group.ru>
2023-06-20 14:26:00 +0300

    add rotate array

---
+++ LeetCode/Easy/Array/Rotate Array/RotateArray.cs
@@ -0,0 +1,19 @@
+﻿public class Solution {
+    public void Rotate(int[] nums, int k) {
+        if(k==0)
+            return;
+        var length = nums.Length;
+        k = k % length;
+        Reverse(nums);
+        Reverse(nums.AsSpan().Slice(0, k));
+        Reverse(nums.AsSpan().Slice(k, length - k));
+    }
+
+    public void Reverse(Span<int> nums){
+        int start = 0;
+        int end = nums.Length - 1;
+        while (start < end){
+            (nums[start], nums[end]) = (nums[end--],nums[start++]);
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,3 @@
+﻿
+new Solution().Rotate(new int[] {5, 4, 3, 2, 1}, 3);
+Console.Write("dsf");
