46e93dd1638767d0f6341c3f4b2f9cb94063ef7a
Svyatoslav Ryumkin <s.ryumkin@of-group.ru>
2023-06-21 09:30:56 +0300

    intersection of an array

---
+++ LeetCode/Easy/Array/IntersectionofTwoArraysII/IntersectArraySolution2.cs
@@ -0,0 +1,20 @@
+﻿public class IntersectArraySolution2 {
+    public int[] Intersect(int[] nums1, int[] nums2)
+    {
+        var result = new List<int>(Math.Max(nums1.Length, nums2.Length));
+
+        var dict = nums1.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+        foreach (var item in nums2)
+        {
+            if (!dict.TryGetValue(item, out var count) || count <= 0)
+            {
+                continue;
+            }
+            result.Add(item);
+            dict[item]--;
+        }
+
+        return result.ToArray();
+    }
+}
