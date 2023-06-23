fea724d22e0071e93fcd80e729ae819f2edf75a3
Svyatoslav Ryumkin <s.ryumkin@of-group.ru>
2023-06-24 00:24:22 +0300

    reverse integer

---
+++ LeetCode/Easy/Strings/Reverse Integer/Reverse String.cs
@@ -0,0 +1,16 @@
+﻿public class ReverseIntegerSln {
+    public int Reverse(int x)
+    {
+        long result = 0;
+        long number = Math.Abs(x);
+        while (number != 0)
+        {
+            result = result * 10 + number % 10;
+            number /= 10;
+            if (result > Int32.MaxValue) return 0;
+        }
+
+        result = x < 0 ? result * -1 : result;
+        return (int)result;
+    }
+}
