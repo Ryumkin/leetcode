fcc8ecf085026f177f912686513f25c3b54c2894
Svyatoslav Ryumkin <s.ryumkin@of-group.ru>
2023-06-24 23:57:55 +0300

    palindrom

---
+++ LeetCode/Easy/Strings/ValidPalindrome/IsPalindrome.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+public class IsPalindromeSln {
+    public bool IsPalindrome(string s)
+    {
+        int left = 0;
+        int right = s.Length-1;
+        while (left<=right)
+        {
+            var l = Char.ToLower(s[left]);
+            var r = Char.ToLower(s[right]);
+            if (!Char.IsNumber(l) && (l < 'a' || l > 'z'))
+            {
+                left++; 
+                continue;
+            }
+            
+            if (!Char.IsNumber(r) && (r < 'a' || r > 'z'))
+            {
+                right--; 
+                continue;
+            }
+
+            if (r != l)
+            {
+                return false;
+            }
+
+            right--;
+            left++;
+        }
+
+        return true;
+    }
+}
