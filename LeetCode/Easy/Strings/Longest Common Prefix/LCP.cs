1b797abd437c5998e78df8f45cc06cea85dc2d26
Svyatoslav Ryumkin <s.ryumkin@of-group.ru>
2023-07-02 18:33:38 +0300

    add longest common prefix

---
+++ LeetCode/Easy/Strings/Longest Common Prefix/LCP.cs
@@ -0,0 +1,37 @@
+﻿public class LongestCommonPrefixSln {
+    public string LongestCommonPrefix(string[] strs) {
+        if (strs == null || strs.Length == 0)
+        {
+            return "";
+        }
+
+        if (strs.Length == 1)
+            return strs[0];
+        var commonPrefix = "";
+        var maxLength = strs[0].Length;
+        int index = 0;
+        while (index < maxLength)
+        {
+            for (int i = 1; i < strs.Length; i++)
+            {
+                if (strs[i].Length < maxLength)
+                {
+                    maxLength = strs[i].Length;
+                }
+
+                if (index >= maxLength)
+                {
+                    return commonPrefix;
+                }
+                if (strs[i][index] != strs[0][index])
+                {
+                    return commonPrefix;
+                }
+            }
+
+            commonPrefix = strs[0].Substring(0, ++index);
+        }
+
+        return commonPrefix;
+    }
+}
