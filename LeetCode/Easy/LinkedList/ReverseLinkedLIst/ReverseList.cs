11d5409cc205da37c277d1dcd3312362256f67c0
Svyatoslav Ryumkin <s.ryumkin@of-group.ru>
2023-07-15 14:36:20 +0300

    reverse list

---
+++ LeetCode/Easy/LinkedList/ReverseLinkedLIst/ReverseList.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml;
+
+public class ReverseListSln {
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode previous = null, next = null;
+
+        while (head != null)
+        {
+            next = head.next;
+            head.next = previous;
+            previous = head;
+            head = next;
+        }
+
+        return previous;
+    }
+}
