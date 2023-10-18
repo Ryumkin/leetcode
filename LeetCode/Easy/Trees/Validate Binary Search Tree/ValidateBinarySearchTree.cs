1a48c7d305a35a7b7de58df7546ac3f1caf6581e
Svyatoslav Ryumkin <s.ryumkin@of-group.ru>
2023-10-18 10:39:03 +0300

    add validation of BST

---
+++ LeetCode/Easy/Trees/Validate Binary Search Tree/ValidateBinarySearchTree.cs
@@ -0,0 +1,35 @@
+﻿public class ValidateBinarySearchTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode? left;
+        public TreeNode? right;
+
+        public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public bool IsValidBST(TreeNode root)
+    {
+        return IsValidBST(root, null, null);
+    }
+    
+    public bool IsValidBST(TreeNode root, int? min, int? max) {
+        if (root == null)
+        {
+            return true;
+        }
+
+        if (root.val <= min || root.val >= max)
+        {
+            return false;
+        }
+
+        return IsValidBST(root.left, min, root.val) && IsValidBST(root.right, root.val, max);
+    }
+}
