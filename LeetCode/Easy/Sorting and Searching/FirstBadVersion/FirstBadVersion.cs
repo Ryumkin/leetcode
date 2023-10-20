cb4d512ffa2fa2813e8e1cfa4c067180024abecf
Svyatoslav Ryumkin <s.ryumkin@of-group.ru>
2023-10-20 22:27:10 +0300

    add first bad version

---
+++ LeetCode/Easy/Sorting and Searching/FirstBadVersion/FirstBadVersion.cs
@@ -0,0 +1,24 @@
+﻿public class FirstBadVersionSln {
+    public int FirstBadVersion(int n)
+    {
+        var left = 1;
+        var right = n;
+        var result = 0;
+
+        while (left <= right)
+        {
+            int middle = left + (right - left) / 2;
+            if (IsBadVersion(middle))
+            {
+                result = middle;
+                right = middle - 1;
+            }
+            else
+                left = middle + 1;
+        }
+
+        return result;
+    }
+
+    public bool IsBadVersion(int middles) => false;
+}
