bbfaed267cc1f28766ccf64bcee1c6c00d1658b6
Svyatoslav Ryumkin <s.ryumkin@of-group.ru>
2023-10-26 13:31:02 +0300

    climb stairs

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,8 +1,1 @@
-﻿var lst1 = new MergeTwoListsSln.ListNode(1);
-lst1.next = new MergeTwoListsSln.ListNode(2);
-lst1.next.next = new MergeTwoListsSln.ListNode(4);
-
-var lst2 = new MergeTwoListsSln.ListNode(1);
-lst2.next = new MergeTwoListsSln.ListNode(3);
-lst2.next.next = new MergeTwoListsSln.ListNode(4);
-Console.Write(new MergeTwoListsSln().MergeTwoLists(lst1, lst2));
+﻿Console.Write(new ClimbStairsSln().ClimbStairs(44));
