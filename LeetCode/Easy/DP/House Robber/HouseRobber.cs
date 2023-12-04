0233dd4f0d8a23ca1241f9f903c003c1b0e9abce
Svyatoslav Ryumkin <s.ryumkin@of-group.ru>
2023-12-04 14:36:34 +0300

    add HouseRobber.cs

---
+++ LeetCode/Easy/DP/House Robber/HouseRobber.cs
@@ -0,0 +1,14 @@
+﻿public class HouseRobber {
+    
+    public int Rob(int[] nums)
+    {
+        var rob1 = 0;
+        var rob2 = 0;
+        foreach (var houseValue in nums)
+        {
+            (rob1, rob2) = (rob2, Math.Max(rob1 + houseValue, rob2));
+        }
+
+        return rob2;
+    }
+}
